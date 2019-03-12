4fb7837c86bb3d079b4373fe41d389cf877ac28e
rodrigobpe <48135762+rodrigobpe@users.noreply.github.com>
2019-03-12 11:00:40 -0300

    Update exercicio_bhaskara.php

--- exercicio_bhaskara.php
+++ exercicio_bhaskara.php
@@ -1,26 +1,21 @@
 <?php   
-
     //valores
     
     print "Digite o Valor de A: ";
     $a= fgets(STDIN);
-
     print "Digite o Valor de B: ";
     $b= fgets(STDIN);
-
     print "Digite o Valor de C: ";
     $c= fgets(STDIN);
 
-
     //delta
-
-    $delta= pow($b,2)-(4*$a*$c);
-    print "$delta";
-
+    
+    $delta= pow($b,2)-4*$a*$c;
+    print "Δ = $delta \n";
+    
     //bhaskara
 
-    $x1= (int)($b+ sqrt($delta))/2*$a;
-    echo "\n$x1";
-
-    $x2= (int)($b- sqrt($delta))/2*$a;
-    echo "\n$x2";
+    $x1= (int)(-$b+ sqrt($delta)) /(2*$a);
+    print "x1 = $x1 \n";
+    $x2= (int)(-$b- sqrt($delta))/(2*$a);
+    print "x2 = $x2 \n";
